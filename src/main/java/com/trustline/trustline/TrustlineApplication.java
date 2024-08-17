998729b109088c47225fb04c534cdefafdbff20e
jerrycaffe <jerrycaffe@gmail.com>
2024-08-17 11:48:58 +0100

    project set up

---
+++ src/main/java/com/trustline/trustline/TrustlineApplication.java
@@ -0,0 +1,13 @@
+package com.trustline.trustline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trustline/trustline/TrustlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustline.trustline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
