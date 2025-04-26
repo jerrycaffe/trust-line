3cf2971ad996cd35a5880dfedc1455c0d186cbfc
jerrycaffe <jerrycaffe@gmail.com>
2025-04-27 00:46:50 +0100

    refactored code for register user and worked on test and other part of the code including firebase config

--- src/main/java/com/trustline/trustline/config/firebase/FirebaseConfig.java
+++ src/main/java/com/trustline/trustline/config/firebase/FirebaseConfig.java
@@ -6,24 +6,24 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
+@Value("${firebase.config.location}")
+private String firebaseLocation;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
 
-        InputStream serviceAccount = new ClassPathResource("firebase/trustline-b92df-7c1a0fdeb463.json").getInputStream();
+        InputStream serviceAccount = new ClassPathResource(firebaseLocation).getInputStream();
         FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
