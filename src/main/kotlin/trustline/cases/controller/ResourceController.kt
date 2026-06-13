1ff9acb77d563d328903630a5d78100cbcb58767
jerrycaffe <jerrycaffe@gmail.com>
2026-06-13 16:39:48 +0100

    updated get resource getting institution id from auth

--- src/main/kotlin/trustline/cases/controller/ResourceController.kt
+++ src/main/kotlin/trustline/cases/controller/ResourceController.kt
@@ -52,11 +52,10 @@ class ResourceController(
 
     @GetMapping
     fun getAll(
-        @RequestParam("institutionId") institutionId: UUID,
         @RequestParam(value = "offset") offset: Int? = 0,
         @RequestParam(value = "limit") limit: Int? = 20,
     ): PagedResponse<ResourceResponseDto> {
-        return resourceService.getAllResources(institutionId, offset!!, limit!!)
+        return resourceService.getAllResources(offset!!, limit!!)
     }
 
     @GetMapping("/incident-type/{incidentTypeId}")
